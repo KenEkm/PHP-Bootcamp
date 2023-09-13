7c57bb957c787957738d7dc2aaa83ac894b3d390
KenEkm <kenankeni@web.de>
2023-09-14 00:52:11 +0200

    some php basics and including multiple php files
    for example to split html and php sections into multiple files for structure and reusability purposes

---
+++ elements/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require("header.php");
+
+include("element.php");
+
+include("element.php");
+
+include_once("footer.php");
+
+?>
--- programm.php
+++ programm.php
@@ -1,8 +1,41 @@
 <?php
 echo "Hallo Welt! <br>";
 
-$name = "Keni"."Beni";
+$name = "Keni "."Beni";
 echo $name."<br>";
 var_dump($name);
+
+//some variable functions
+var_dump(empty($name));
+var_dump(isset($name));
+
+echo("<br>");
+
+//if else
+if(!empty($name)){
+    echo("Name ist definiert.");
+} else {
+    echo("Name ist nicht definiert.");
+}
+echo("<br>");
+
+//some string functions
+echo str_replace("Beni", "Benny", $name);
+echo strpos($name, "Beni");
+
+echo("<br>");
+
+$i = 0;
+
+//while loop
+while($i < 5){
+    echo("Hallo <br>");
+    $i++;
+}
+
+//for loop
+for($i = 0; $i < 5; $i++){
+    var_dump($i);
+}
 
 ?>
