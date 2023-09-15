b4bb9d2b25dff035fd62014caedbc6978720856b
KenEkm <kenankeni@web.de>
2023-09-15 22:09:43 +0200

    using set namespaces

---
+++ PhpCourseContent/Namespaces/programm.php
@@ -0,0 +1,14 @@
+<?php
+
+require("./Blog/PostInterface.php");
+require("./Blog/Post.php");
+require("./Forum/Post.php");
+
+$post = new Blog\Post();
+var_dump($post);
+echo("<br>");
+
+$post2 = new Forum\Post();
+var_dump($post2);
+
+?>
