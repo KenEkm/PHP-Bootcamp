04265bb4a722d29feb0e26ddb20f3ec532837009
KenEkm <kenankeni@web.de>
2023-09-15 21:41:45 +0200

    accessing db with new user without admin rights

--- BlogProject/database.php
+++ BlogProject/database.php
@@ -1,9 +1,10 @@
 <?php
+require("dbAccess.php");
 //Verbindung zur lokalen DB mit PDO Schnittstelle aufbauen
 $pdo = new PDO(
     'mysql:host=localhost;dbname=blog;charset=utf8', 
-    'root', 
-    ''
+    'blog', 
+    getPw()
 );
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);  //um SQL-Injection zu verhindern
 
