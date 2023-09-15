4e586119a17e646c3a7fde8a3bb1e3fa956de23d
KenEkm <kenankeni@web.de>
2023-09-15 22:02:58 +0200

    restructuring file directory for better overview, adding namespaces

---
+++ PhpCourseContent/Namespaces/Blog/Post.php
@@ -0,0 +1,9 @@
+<?php
+namespace Blog; //Namespace wie Ordner, verhindert gleiche Klassenbezeichnungen in versch Ordnern
+
+class Post implements PostInterface
+{
+    public $title;
+}
+
+?>
---
+++ PhpCourseContent/Namespaces/Forum/Porst.php
@@ -0,0 +1,9 @@
+<?php
+namespace Forum;
+
+class Post
+{
+    public $title;
+}
+
+?>
