bea2ef7d94683355ab3e6ade41cd90f73e982f60
KenEkm <kenankeni@web.de>
2023-09-16 17:12:54 +0200

    adding Model layer, storing fetched data as object instead of array

--- BlogProject/pages/post.php
+++ BlogProject/pages/post.php
@@ -16,11 +16,11 @@ $post = $postsRepository->fetchPost($id);
 
 <div class="card text-bg-dark mb-3">
   <div class="card-header">
-    <h3><?php echo $post["title"]; ?></h3>
+    <h3><?php echo $post->title; ?></h3>
   </div>
   <div class="card-body">
     <blockquote class="blockquote mb-0">
-      <p><?php echo nl2br($post["content"]); ?></p>   <!-- nl2br wendelt Zeilenumbrüche in breaks um. -->
+      <p><?php echo nl2br($post->content); ?></p>   <!-- nl2br wendelt Zeilenumbrüche in breaks um. -->
       <footer class="blockquote-footer">Author</footer>
     </blockquote>
   </div>
--- BlogProject/src/Post/PostsRepository.php
+++ BlogProject/src/Post/PostsRepository.php
@@ -21,7 +21,13 @@ class PostsRepository
     function fetchPost($id){
         $stmt = $this->pdo->prepare("SELECT * FROM `posts` WHERE id = ?"); //prepare Statements gegen SQL-Inj
         $stmt->execute([$id]);   //? aus prepare Statement wird hier gefüllt
-        return $stmt->fetch();
+        $postArray = $stmt->fetch();
+
+        $post = new PostModel();
+        $post->id = $postArray["id"];
+        $post->title = $postArray["title"];
+        $post->content = $postArray["content"];
+        return $post;
 
         /*  Achtung SQL-Injection Gefahr, da nicht überprft ob $id Parameter oder sql Abfrage ist
         $q = $pdo->query("SELECT * FROM `posts` WHERE id='{$id}'");
