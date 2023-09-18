36750e20210536f4c4799aaf17450e14ed4e6149
KenEkm <kenankeni@web.de>
2023-09-18 17:58:31 +0200

    course topic: forms, sending arrays

--- PhpCourseContent/Forms/form.php
+++ PhpCourseContent/Forms/form.php
@@ -4,13 +4,25 @@
   </head>
 
   <body>
-    <form method="GET" action="submit.php">
+    <!-- use method Get oder Post -->
+    <form method="POST" action="submit.php">
       <strong>Vorname:</strong>
       <input type="text" name="firstname" />
       <br />
       <strong>Nachname:</strong>
       <input type="text" name="surname" />
       <br />
+
+      <strong>Tags - Elektronik:</strong>
+      <input type="checkbox" name="tags[elektronik]" value="Elektronik" />
+      <br />
+      <strong>Tags - Obst:</strong>
+      <input type="checkbox" name="tags[obst]" value="Obst" />
+      <br />
+      <strong>Tags - Programmierung:</strong>
+      <input type="checkbox" name="tags[programmierung]" value="Programmierung" />
+      <br />
+
       <input type="submit" />
     </form>
   </body>
