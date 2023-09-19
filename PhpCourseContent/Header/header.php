149c8a8e4fe61b8b764757f6c759f99d3e3a2349
KenEkm <kenankeni@web.de>
2023-09-19 18:06:43 +0200

    course topic: header

---
+++ PhpCourseContent/Header/header.php
@@ -0,0 +1,14 @@
+<?php
+    header("Content-Type: text/html");
+    header("test: Header Test.");
+    //header("Location: index.php"); Weiterleidung zu anderem file
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Hallo Welt!</h1>
+    </body>
+</html>
